22a3330e95b53debbadd040ba0abd347f3976b53
Lars Bakker <Lars.bakker@casema.nl>
2023-05-05 11:06:11 +0200

    Add Basic Functionality

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("Hello World")
+function opponentChoice() // Generates a pseudo-random opponent choice
+{
+    let choice = "";
+    let randomNum = Math.floor((Math.random() * 3) + 1);
+
+    randomNum === 1 ? choice = "rock" : randomNum === 2 ? choice = "paper" : randomNum ===  3 ? choice = "scissors" : choice = "Error";
+
+    return choice;
+}
+
+function rpsGame() // Plays  a round of the rock, paper, scissors game (rps)
+{
+    let opponentMove = opponentChoice();
+    playerChoice = prompt("What is your move? ");
+    playerChoice.toLowerCase()
+
+    if (playerChoice === opponentMove)
+    {
+        return `You chose: ${playerChoice}, Opponent chose: ${opponentMove}. It's a tie.`;
+    }
+    else if (playerChoice === "rock" && opponentMove === "scissors" || playerChoice === "scissors" && opponentMove === "paper" || playerChoice === "paper" && opponentMove === "rock") 
+    {
+        return `You chose: ${playerChoice}, Opponent chose: ${opponentMove}. You win!`;
+    }
+    else {
+        return `You chose ${playerChoice}, Opponent chose: ${opponentMove}. You lose...`;
+    }
+}
+
+function game(roundAmount) // Plays the game for your chosen amount of times
+{
+    for (let i = 0; i < roundAmount; i++)
+    {
+        console.log(rpsGame());
+    }
+}
+
+game(5);
